50532e6581be24317e43e9ac1f4ef8b823c3fa6c
SabrinaJewson <sejewson@gmail.com>
2026-02-06 02:46:01 +0000

    Do embeds ourselves

--- src/poll.rs
+++ src/poll.rs
@@ -63,28 +63,25 @@ async fn consume_post(
     discord: &serenity::Context,
     data: &Data,
     last_updated: chrono::DateTime<chrono::FixedOffset>,
-    post: bsky::feed::defs::FeedViewPostData,
+    mut post: bsky::feed::defs::FeedViewPostData,
 ) -> anyhow::Result<chrono::DateTime<chrono::FixedOffset>> {
+    let record_data = replace(&mut post.post.record, bsky_sdk::api::types::Unknown::Null);
     let record_data =
-        bsky_sdk::api::app::bsky::feed::post::RecordData::try_from_unknown(post.post.data.record)
+        bsky_sdk::api::app::bsky::feed::post::RecordData::try_from_unknown(record_data)
             .context("extracting post record data")?;
 
-    if *record_data.created_at.as_ref() <= last_updated {
+    let created_at = *record_data.created_at.as_ref();
+    if created_at <= last_updated {
         return Ok(last_updated);
     }
 
-    let author = &post.post.data.author;
-    let rkey = extract_rkey(&post.post.data.uri, &author.did)?;
-    let message = format!(
-        "https://bsky.app/profile/{}/post/{}",
-        author.handle.as_str(),
-        rkey,
-    );
+    let message = make_message(&mut post, record_data)?;
 
     struct Channel {
         channel: NonZero<u64>,
     }
 
+    let author = &post.post.data.author;
     let author_did = author.did.as_str();
     let mut channels = sqlx::query_as!(
         Channel,
@@ -100,12 +97,70 @@ async fn consume_post(
     {
         let channel_id = serenity::ChannelId::from(channel.channel);
         channel_id
-            .say(discord, &message)
+            .send_message(discord, message.clone())
             .await
             .context("sending message")?;
     }
 
-    Ok(*record_data.created_at.as_ref())
+    Ok(created_at)
+}
+
+fn make_message(
+    post: &mut bsky::feed::defs::FeedViewPostData,
+    record_data: bsky::feed::post::RecordData,
+) -> anyhow::Result<CreateMessage> {
+    let author = &post.post.data.author;
+    let handle = author.handle.as_str();
+    let rkey = extract_rkey(&post.post.data.uri, &author.did)?;
+    let post_url = format!("https://bsky.app/profile/{handle}/post/{rkey}");
+
+    let mut embed_author = CreateEmbedAuthor::new(match &author.display_name {
+        Some(name) => format!("{name} ({handle})"),
+        None => handle.to_owned(),
+    });
+    embed_author = embed_author.url(format!("https://bsky.app/profile/{handle}"));
+    if let Some(avatar) = &author.avatar {
+        embed_author = embed_author.icon_url(avatar);
+    }
+    let mut embed = CreateEmbed::new()
+        .author(embed_author)
+        .colour(Colour(0x1183fe))
+        .description(escape_markdown(&record_data.text))
+        .timestamp(*record_data.created_at.as_ref())
+        .url(&post_url);
+
+    let post_embeds = post.post.data.embed.take();
+    let mut images = post_embeds.into_iter().flat_map(|embed| match embed {
+        bsky_sdk::api::types::Union::Refs(
+            bsky::feed::defs::PostViewEmbedRefs::AppBskyEmbedImagesView(embed),
+        ) => embed.data.images,
+        _ => Vec::new(),
+    });
+
+    if let Some(image) = images.next() {
+        embed = embed.image(image.data.fullsize);
+    }
+
+    let mut message = CreateMessage::default()
+        .content(format!("<{post_url}>"))
+        .embed(embed);
+
+    for image in images {
+        message = message.add_embed(CreateEmbed::new().url(&post_url).image(image.data.fullsize));
+    }
+
+    Ok(message)
+}
+
+fn escape_markdown(s: &str) -> String {
+    let mut res = Vec::new();
+    for byte in s.bytes() {
+        if let b'*' | b'_' | b'>' | b'`' | b'[' | b'-' | b'#' = byte {
+            res.push(b'\\');
+        }
+        res.push(byte);
+    }
+    String::from_utf8(res).unwrap()
 }
 
 fn extract_rkey<'a>(uri: &'a str, author_did: &Did) -> anyhow::Result<&'a str> {
@@ -136,7 +191,12 @@ use chrono::TimeDelta;
 use futures_util::FutureExt;
 use futures_util::TryStreamExt;
 use poise::serenity_prelude as serenity;
+use poise::serenity_prelude::Colour;
+use poise::serenity_prelude::CreateEmbed;
+use poise::serenity_prelude::CreateEmbedAuthor;
+use poise::serenity_prelude::CreateMessage;
 use std::fmt::Debug;
+use std::mem::replace;
 use std::num::NonZero;
 use std::panic::AssertUnwindSafe;
 use std::time::Duration;
